34bdfcaea152023b7aabd4c16e7d5901160a0b5c
Ivan Grishaev <ivan@grishaev.me>
2025-12-20 19:03:43 +0300

    udpate ch2

--- ch02/python.py
+++ ch02/python.py
@@ -23,3 +23,24 @@ profile.extra_data = {
     "twitter_id": 2477313462,
 }
 profile.save()
+
+from collections import UserDict
+
+class TrackingDict(UserDict):
+
+    def __init__(self, data):
+        super().__init__(data)
+        self._is_changed = False
+
+    def __delitem__(self, key):
+        self._is_changed = True
+        super().__delitem__(key)
+
+    def __setitem__(self, key, value):
+        self._is_changed = True
+        self.data[key] = value
+
+data = TrackingDict({"foo": 1})
+data._is_changed # False
+data["abc"] = 3
+data._is_changed # True
